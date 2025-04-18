90aa3b66a8d13da617468bef75e6cbf7cab034b7
Swastik <swastikyadav1237@gmail.com>
2025-04-18 22:35:31 +0530

    nav items added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Canvas } from "@react-three/fiber";
+import "remixicon/fonts/remixicon.css";
 import React from "react";
 import "./style.css";
 import { Environment, OrbitControls, ScrollControls } from "@react-three/drei";
@@ -7,8 +8,33 @@ import { div } from "three/tsl";
 const App = () => {
   return (
     <div className="w-full h-full">
+      <div className="navbar w-full fixed flex items-center justify-center ">
+        <div className="flexdiv flex pt-8 pb-1 items-center justify-center gap-4 font-sans  text-[1vw]  border-b border-[#ffffff6f] text-[#ffffffa9] w-fit">
+          <h4>
+            <i class="ri-apple-fill text-sm text-[#ffffffc0]"></i>
+          </h4>
+          <h4>Store</h4>
+          <h4>Mac</h4>
+          <h4>iPad</h4>
+          <h4>iPhone</h4>
+          <h4>Watch</h4>
+          <h4>Airpods</h4>
+          <h4>TV & Home</h4>
+          <h4>Entertainment</h4>
+          <h4>Accessories</h4>
+          <h4>Support</h4>
+          <h4>
+            <i class="ri-search-line text-sm text-[#ffffffc0]"></i>
+          </h4>
+          <h4>
+            <i class="ri-shopping-bag-line text-sm text-[#ffffffc0]"></i>
+          </h4>
+        </div>
+      </div>
       <div className="absolute left-[50%] translate-x-[-50%] top-[14.5vh] flex flex-col items-center gap-1 text-white">
-        <h3 className="glow text-6xl tracking-tighter font-[300]">macbook pro.</h3>
+        <h3 className="glow text-6xl tracking-tighter font-[300]">
+          macbook pro.
+        </h3>
         <h5 className="text-base">Oh so pro!</h5>
         <p className="text-center w-3/6 text-xs">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt a

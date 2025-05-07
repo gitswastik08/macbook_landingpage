9643d496861bb4fffae32ce003034439d7760dc8
Swastik <swastikyadav1237@gmail.com>
2025-05-07 13:45:45 +0530

    loader added

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,32 +1,15 @@
-// components/Loader.jsx
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Html, useProgress } from "@react-three/drei";
+import { FaApple } from "react-icons/fa";
 
 const Loader = () => {
-  const { progress } = useProgress();
-  const [done, setDone] = useState(false);
-
-  useEffect(() => {
-    if (progress >= 100) {
-      // Thoda delay so animation completes nicely
-      const timeout = setTimeout(() => {
-        setDone(true);
-      }, 500);
-      return () => clearTimeout(timeout);
-    }
-  }, [progress]);
-
-  if (done) return null;
+  const { progress } = useProgress(); // progress is percentage
 
   return (
     <Html center>
-      <div className="apple-loader-container">
-        <div className="apple-logo">
-        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0 0 50 50"
-style="fill:#FFFFFF;">
-<path d="M 44.527344 34.75 C 43.449219 37.144531 42.929688 38.214844 41.542969 40.328125 C 39.601563 43.28125 36.863281 46.96875 33.480469 46.992188 C 30.46875 47.019531 29.691406 45.027344 25.601563 45.0625 C 21.515625 45.082031 20.664063 47.03125 17.648438 47 C 14.261719 46.96875 11.671875 43.648438 9.730469 40.699219 C 4.300781 32.429688 3.726563 22.734375 7.082031 17.578125 C 9.457031 13.921875 13.210938 11.773438 16.738281 11.773438 C 20.332031 11.773438 22.589844 13.746094 25.558594 13.746094 C 28.441406 13.746094 30.195313 11.769531 34.351563 11.769531 C 37.492188 11.769531 40.8125 13.480469 43.1875 16.433594 C 35.421875 20.691406 36.683594 31.78125 44.527344 34.75 Z M 31.195313 8.46875 C 32.707031 6.527344 33.855469 3.789063 33.4375 1 C 30.972656 1.167969 28.089844 2.742188 26.40625 4.78125 C 24.878906 6.640625 23.613281 9.398438 24.105469 12.066406 C 26.796875 12.152344 29.582031 10.546875 31.195313 8.46875 Z"></path>
-</svg>
-        </div>
+      <div className="flex flex-col items-center justify-center mt-[20vh] gap-4">
+        <FaApple className="text-white text-5xl animate-bounce" />
+       
       </div>
     </Html>
   );
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,34 +1,42 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { Environment, OrbitControls, ScrollControls } from "@react-three/drei";
+import { Environment, ScrollControls } from "@react-three/drei";
 import MacContainer from "../MacContainer";
+import Loader from "../components/Loader";
 
 const Homepage = () => {
   return (
-    <div className="homepage w-full h-full">
-      <div className="w-full absolute  left-[50%] translate-x-[-50%] top-[13.5vh] flex flex-col items-center justify-start gap-1  text-white">
+    <div className="homepage w-full h-full relative">
+      {/* Top Text & Image */}
+      <div className="w-full absolute left-[50%] translate-x-[-50%] top-[13.5vh] flex flex-col items-center justify-start gap-1 text-white z-10 pointer-events-none">
         <div className="absolute top-1">
-          <h1 className="glow" >macbook pro</h1>
-       
+          <h1 className="glow">macbook pro</h1>
         </div>
         <img
           className="w-[80vw]"
           src="https://www.apple.com/in/macbook-pro/images/overview/welcome/hero_apple_intelligence_headline__d3q0g47xl682_medium_2x.png"
           alt=""
         />
-     
       </div>
-      <Canvas camera={{ fov: 20, position: [10, 0, 220] }}>
-        <Environment
-          files={[
-            "https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_09_4k.exr",
-          ]}
-        />
-        <ScrollControls pages={3}>
-          <MacContainer />
-        </ScrollControls>
-      </Canvas>
-      <div className="h-10 w-12 z-30 absolute left-[50%] top-[80%] translate-x-[-50%] translate-y-[-50%] flex flex-col items-center gap-4 ">
+
+      {/* Canvas with Loader inside */}
+      <div className="w-full h-screen">
+        <Canvas camera={{ fov: 20, position: [10, 0, 220] }}>
+          <Suspense fallback={<Loader />}>
+            <Environment
+              files={[
+                "https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_09_4k.exr",
+              ]}
+            />
+            <ScrollControls pages={3}>
+              <MacContainer />
+            </ScrollControls>
+          </Suspense>
+        </Canvas>
+      </div>
+
+      {/* Buy Button */}
+      <div className="h-10 w-12 z-30 absolute left-[50%] top-[80%] translate-x-[-50%] translate-y-[-50%] flex flex-col items-center gap-4">
         <button className="bg-[#2177c8] py-1 px-6 text-lg font-macFont text-white rounded-2xl">
           Buy
         </button>
